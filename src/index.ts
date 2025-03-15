450b72f36433c51ca3ea03ba6c1ee109dbc8f43d
Rohan Hussain <rohanhussain1@yahoo.com>
2025-03-16 00:27:13 +0500

    added @hono/zod-validator

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import * as z from 'zod'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post("/", zValidator('json', z.object({body: z.string()})), (c) => {
+  const { body } = c.req.valid('json');
+  return c.text("Hello Hono!")
 })
 
 export default app
