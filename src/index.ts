65288052230ccf8d22de35c15223955008929e27
Rohan Hussain <rohanhussain1@yahoo.com>
2025-03-16 00:48:15 +0500

    added OpenAPI spec support

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,83 @@
-import { zValidator } from '@hono/zod-validator'
-import { Hono } from 'hono'
-import * as z from 'zod'
+import { Hono } from "hono";
+import { z } from "@hono/zod-openapi";
+import { describeRoute, generateSpecs, openAPISpecs } from "hono-openapi";
+import { resolver, validator as zValidator } from "hono-openapi/zod";
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-app.post("/", zValidator('json', z.object({body: z.string()})), (c) => {
-  const { body } = c.req.valid('json');
-  return c.text("Hello Hono!")
-})
+const responseSchema = z.string().openapi({ example: "Hello Steven!" });
 
-export default app
+app.get(
+  "/",
+  describeRoute({
+    description: "Say hello to the user",
+    responses: {
+      200: {
+        description: "Successful greeting response",
+        content: {
+          "text/plain": {
+            schema: resolver(responseSchema),
+          },
+        },
+      },
+    },
+  }),
+  (c) => {
+    return c.text("Hello World!");
+  }
+);
+
+const requestSchema = z.object({ body: z.string() });
+app.post(
+  "/",
+  describeRoute({
+    description: "Sample post endpoint",
+    requestBody: {
+      description: "JSON request body",
+      required: true,
+      content: {
+        "application/json": {
+          schema: resolver(requestSchema),
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "text/plain": {
+            schema: resolver(responseSchema),
+          },
+        },
+      },
+    },
+  }),
+  zValidator("json", requestSchema),
+  (c) => {
+    const { body } = c.req.valid("json");
+    return c.text("Hello Hono!");
+  }
+);
+
+app.get(
+  "/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: "Hono",
+        version: "1.0.0",
+        description: "API for greeting users",
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+          description: "Local server",
+        },
+      ],
+    },
+  })
+);
+
+
+export default app;
